e0f85a6c17052b7a49d265553810411cbc488372
Yasmin-AlGhamdi <82459882+Yasmin-AlGhamdi@users.noreply.github.com>
2021-04-28 14:51:36 +0300

    Add files via upload

---
+++ PreDirIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿//#define WITHRETURN
+//#undef WITHRETURN
+//#define TESTCODE
+
+using System;
+
+#if !WITHRETURN
+#warning You must use WITHRETURN in this situation
+#endif
+namespace PreDirIntro
+{
+    class Program
+    {
+#if WITHRETURN
+
+        public static int sum(int first, int second)
+        {
+            return first + second;
+        }
+#else
+        public static void sum(int first, int second)
+        {
+            Console.WriteLine(first + second);
+        }
+#endif
+        static void Main(string[] args)
+        {
+#if DEBUG // predefined symbol
+            Console.WriteLine("Debug mode..");
+//#endif
+//#if TESTCODE
+            int a = 10;
+            if (a >= 10) Console.WriteLine("XXX");
+#endif
+
+
+#if WITHRETURN
+            int result = sum(2, 7);
+            Console.WriteLine(result);
+#else
+            sum(2,7);
+#endif
+
+            Console.ReadKey();
+        }
+    }
+}
